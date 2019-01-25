efa8a0e0d6ebae36abbe955640a4cf45464d78b6
Julián Fernández Reyes <jfernandezreyes@anac.gob.ar>
2019-01-25 17:29:20 -0300

    #4 Handling Errors & Improving the Project Setup

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const app = express();
 
+app.use(require("morgan")("tiny"));
+
 app.use("/products", require("./api/routes/products"));
 app.use("/orders", require("./api/routes/orders"));
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+});
 
 module.exports = app;
